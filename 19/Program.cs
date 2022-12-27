50c4d00f43563fd1b69fb88428c726547287e34c
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-27 23:42:16 +0300

    finish task 19

---
+++ 19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+int num, temp, reverse = 0, rem;
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;
+while(num > 0)
+{
+    rem = num % 10;
+    reverse = reverse * 10 + rem;
+    num = num / 10;
+}
+if(reverse==temp)
+Console.WriteLine("palindrome");
+else
+Console.WriteLine("no palindrome");
